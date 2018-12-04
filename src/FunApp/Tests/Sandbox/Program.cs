c662589d116156769320f7fc4fee42ada788b5b7
Stefan Stoyanov <stefan_1991_stoyanov@abv.bg>
2018-12-04 17:25:00 +0200

    Created Category and Joke models and created crawler for them from dir.bg

--- src/FunApp/Tests/Sandbox/Program.cs
+++ src/FunApp/Tests/Sandbox/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using AngleSharp;
+using AngleSharp.Parser.Html;
 using FunApp.Data;
+using FunApp.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,8 +32,43 @@ namespace Sandbox
 
         private static void SandboxCode(IServiceProvider serviceProvider)
         {
-            var db = serviceProvider.GetService<FunAppContext>();
-            Console.WriteLine(db.Users.Count());
+            var dbContext = serviceProvider.GetService<FunAppContext>();
+
+            //Getting the joke from dir.bg with AngleSharp   
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var webClient = new WebClient() {Encoding = Encoding.GetEncoding("windows-1251")};
+            var parser = new HtmlParser();
+
+            for (int i = 1; i < 10000; i++)
+            {
+                var url = "http://fun.dir.bg/vic_open.php?id=" + i;
+                var html = webClient.DownloadString(url);
+
+                var document = parser.Parse(html);
+                var jokeContent = document.QuerySelector("#newsbody")?.TextContent.Trim();
+                var categoryName = document.QuerySelector(".tag-links-left a")?.TextContent.Trim();
+
+                if (!string.IsNullOrWhiteSpace(jokeContent) && !string.IsNullOrWhiteSpace(categoryName))
+                {
+                    var category = dbContext.Categories.FirstOrDefault(x => x.Name == categoryName);
+                    if (category == null)
+                    {
+                        category = new Category() {Name = categoryName};
+                    }
+
+                    var joke = new Joke()
+                    {
+                        Category = category,
+                        Content = jokeContent
+                    };
+
+                    dbContext.Jokes.Add(joke);
+                    dbContext.SaveChanges();
+                }
+
+                Console.WriteLine($"{i} => Joke number {i} added");
+            }          
+            //
 
             // TODO: Code here
         }
